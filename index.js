cedc7aafad08ef6aafae9f4e8e5cb10869c52992
azizshahid <azizshahid222@gmail.com>
2026-09-12 13:14:44 +0530

    scroll effects

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const section = document.querySelectorAll('section');
-const navLinks = document.querySelectorAll('.nav_links a');
-const spyObserver = new IntersectionObserver(callbackFuntion, { threshhold: 0.5});
+const section = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav_links a");
+const spyObserver = new IntersectionObserver(callbackFuntion, {
+  rootMargin: "-45% 0px -45% 0px",
+  threshold: 0,
+});
 
-section.forEach(section => spyObserver.observe(section));
+section.forEach((section) => spyObserver.observe(section));
 
 function callbackFuntion(entries) {
-    entries.forEach(entry => {
-        if(entry.isIntersecting  === true) {
-           let id = entry.target.id;
-           sctnId = 
-           console.log(`id is ${id}`);
-        }
-    })
+  entries.forEach((entry) => {
+    if (entry.isIntersecting === true) {
+      let id = entry.target.id;
+      navLinks.forEach((link) => link.classList.remove("active"));
+      let matchLink = document.querySelector(`.nav_links a[href="#${id}"]`);
+      matchLink.classList.add("active");
+      if (matchLink) {
+      matchLink.classList.add("active"); }
+    }
+  });
 }
